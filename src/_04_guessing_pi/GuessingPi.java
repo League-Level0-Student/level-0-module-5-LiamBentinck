9f048203e47509303a95f1f216f881218a5d170d
LiamBentinck <50850141+LiamBentinck@users.noreply.github.com>
2020-03-27 16:55:57 -0700

    on snowflake

--- src/_04_guessing_pi/GuessingPi.java
+++ src/_04_guessing_pi/GuessingPi.java
@@ -8,6 +8,12 @@ import javax.swing.JOptionPane;
 
 public class GuessingPi {
 	// 1. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	
+	String pi = "3.14159";
+	System.out.println(pi.charAt(0));
+	System.out.println(pi.charAt(1));
+	System.out.println(pi.charAt(2));
 
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
@@ -18,11 +24,21 @@ public class GuessingPi {
 
 		// 9. If you want to give the user more than one chance to guess,
 		//    put a for loop around steps 4-8.
-
+	for (int i = 3; i < pi.length(); i++) {
+		String nextpi = JOptionPane.showInputDialog("next digit of pi please.");
+		if (nextpi.charAt(0) == pi.charAt(i)) {
+			System.out.println("You are correct!");
+		}
+		else {
+			System.out.println("incorrect.");
+			break;
+		}
+		
+		
 			// 4. Create a for loop that will step through each digit of pi (steps 5-8)
 			//    NOTE: The number of digits in your string will be
 			//       yourStringVariable.length()
-
+	
 				// 5. Ask the user for the NEXT digit of pi.
 
 				// 6. Compare the user's input to the next digit of your pi variable 
@@ -32,8 +48,9 @@ public class GuessingPi {
 					// 7. If they are correct, print out "correct".
 
 					// 8. If they are not, print out "incorrect" and tell them
-					//    to start over. Use 'break;' to break out of the loop.
-
+					//to start over. Use 'break;' to break out of the loop.
+	}
+}
 }
 
 
